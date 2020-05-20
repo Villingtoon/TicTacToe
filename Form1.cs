c74bdfef6a1988ac9e55f616abea75ea3cd7eadf
Noob gamer <legofanslv@gmail.com>
2020-05-20 18:42:28 +0300

    ver 18:42

--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,47 @@ using System.Windows.Forms;
 
 namespace TicTacToe
 {
-    public partial class Form1 : Form
+    public partial class TicTacToe : Form
     {
-        public Form1()
+        bool player1 = true;
+        public TicTacToe()
         {
             InitializeComponent();
+            InitializeGrid();
+            InitializeCells();
+        }
+
+        private void InitializeGrid()
+        {
+            Grid.BackColor = Color.LightCoral;
+            Grid.CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset;
+        }
+
+        private void InitializeCells()
+        {
+            string labelName;
+            for (int i = 1; i <= 9; i++)
+            {
+                labelName = "label" + i;
+                Grid.Controls[labelName].Text = string.Empty;
+            }
+        }
+
+        private void Player_Click(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            //label1.Text = "X";
+
+            if (player1 == true)
+            {
+                label.Text = "X";
+                player1 = false;
+            }
+            else
+            {
+                label.Text = "O";
+                player1 = true;
+            }
         }
     }
 }
